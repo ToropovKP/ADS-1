c9120042a11dcb726e4d72b139baccefb8c7527a
ToropovKP <44004844+ToropovKP@users.noreply.github.com>
2021-03-24 16:30:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int right = size - 1, left = 0, count = 0,mid;
+    bool flag = false;
+    
+    while ((left <= right) && (flag != true)) {
+        mid = (left + right) / 2;
+        if (arr[mid] == value) flag = true; 
+        if (arr[mid] > value) right = mid - 1; 
+        else left = mid + 1;
+    }
+    if (flag){
+        return mid; 
+    } 
+    else return-1;
 }
