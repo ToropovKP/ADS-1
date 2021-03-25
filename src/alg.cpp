1a2902afe83b53617f27bd6fafec20d966412351
ToropovKP <44004844+ToropovKP@users.noreply.github.com>
2021-03-25 08:38:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,13 @@ int cbinsearch(int *arr, int size, int value) {
   int right = size - 1, left = 0, count = 0, mid;
     bool flag = false;
     while ((left <= right) && (flag != true)) {
-        mid = (left + right) / 2;
-        if (arr[mid] == value) flag = true; 
-        if (arr[mid] > value) right = mid - 1; 
+        mid = (left + right) / 2
+        if (arr[mid] == value) flag = true;
+        if (arr[mid] > value) right = mid - 1;
         else left = mid + 1;
     }
     if (flag){
-        return mid; 
-    } 
+        return mid;
+    }
     else return 0;
 }
